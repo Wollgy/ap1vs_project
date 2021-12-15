58fe71a903907fc765270075c9a0fcdcb918b4f6
Wollgy <42422354+Wollgy@users.noreply.github.com>
2021-12-15 19:11:44 +0100

    Připraveny testy třídy Trojuhelnik

---
+++ tests/test_trojuhelnik.py
@@ -0,0 +1,36 @@
+"""Obsahuje Unit testy pro modul bod."""
+from unittest import TestCase
+from bod import Bod
+from trojuhelnik import Trojuhelnik
+
+
+class TestTrojuhelnik(TestCase):
+    """Spouští testy funkcí pro práci s trojúhelníkem."""
+
+    def test_a(self):
+        # TODO
+        self.fail()
+
+    def test_b(self):
+        # TODO
+        self.fail()
+
+    def test_c(self):
+        # TODO
+        self.fail()
+
+    def test_je_sestrojitelny(self):
+        # TODO
+        self.fail()
+
+    def test_obvod(self):
+        # TODO
+        self.fail()
+
+    def test_obsah(self):
+        # TODO
+        self.fail()
+
+    def test_je_pravouhly(self):
+        # TODO
+        self.fail()
